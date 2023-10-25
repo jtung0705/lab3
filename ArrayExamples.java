874b7f08afd292d30e1dc2488a253539a0629eac
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-10-25 15:10:50 -0700

    updated array class

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -14,7 +14,10 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      arr[i] = newArray[arr.length - i - 1]; // assume upper is 10
+      // 0 = 10 - 0 - 1
+      /* 0
+      */
     }
     return arr;
   }
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,24 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void avgLowest() {
+    double[] input1 = { 3.0,4.0,5.0 };
+    double s = ArrayExamples.averageWithoutLowest(input1);
+    assertArrayEquals(new double[]{5.0,4.0,3.0}, new double[]{4.5});
+	}
 
   @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  public void testReverseInPlace2_5() {
+    int[] input1 = { 2, 3, 4, 5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 4, 3, 2}, input1);
+  }
+
+  @Test
+  public void testReversed2_5() {
+    int[] input1 = { 2,3,4,5};
+    int[] inp2 = ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{5,4,3,2 }, inp2);
   }
 }
